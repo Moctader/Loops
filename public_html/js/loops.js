c5ff43f0428b575d75048d2293e3f700a5b478fe
Moctader <54657677+Moctader@users.noreply.github.com>
2021-02-09 15:32:58 +0200

    Add files via upload

---
+++ public_html/js/loops.js
@@ -0,0 +1,55 @@
+/* 
+    Created on : May, 2019 
+    Author     : Liisa Auer, Oulu University of Applied Sciences
+    Licence    : CC-BY-4.0
+*/
+
+/*
+ * Student name :
+ * Started date :
+ * Ended   date :
+ * 
+ */
+
+function l01() {    
+
+}
+
+function l02() {    
+
+}
+
+function l03() {
+
+}
+
+function l04() {
+
+}
+
+function l05() {
+
+}
+
+// copy and paste here the random number generator function 
+// and use it in the following functions
+
+
+
+function l06() {   
+
+}
+
+function l07() {
+
+}
+
+function l08() {
+
+}
+
+function l09() {
+    const space = ";&nbsp;&nbsp;";
+
+}
+
