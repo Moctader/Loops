35af7be078d2a29c12dd1f39d844b44d6cdd7642
Moctader <moctader.golam@gmail.com>
2021-02-10 00:42:25 +0200

    not finnished yet

--- public_html/js/loops.js
+++ public_html/js/loops.js
@@ -13,30 +13,124 @@
 
 function l01() {    
 
+    let name1 = document.getElementById("name1").value
+
+    for (i =0; i<5; i++){
+        document.getElementById("first_name").innerHTML += name1 + "<br>";
+
+    }
+
+    let j = 0;
+    let name2 = document.getElementById("name2").value
+    while (j < 5){
+        document.getElementById("last_name").innerHTML += name2 + "<br>";
+        j++;
+    }
 }
 
 function l02() {    
 
+    for (k = 5; k<=50; k+=5)
+    document.getElementById("five").innerHTML += k +" "
+
+    let numbers =6;
+    while(numbers <=60){
+        document.getElementById("six").innerHTML += numbers +" "
+        numbers += 6
+    }
+
 }
 
 function l03() {
+    let amount = Number(document.getElementById("amount").value)
+
+    let stars1 = document.getElementById("stars1");
+    let stars2 = document.getElementById("stars2");
+
+    document.getElementById("stars1").innerHTML=""
+    document.getElementById("stars2").innerHTML=""
+
+    star =" ";
+    for (i=0; i< amount; i++){
+        star = star + "*"
+        document.getElementById("stars1").innerHTML += star + "<br>"
+    }
+
+
+// while loop using In the first row there is one star,in the second row two stars, etc. until in the last row
+    star= " "
+    j = 0;
+    while (j < amount){
+        star += "*"
+        document.getElementById("stars2").innerHTML += star + "<br>"
+        j++;
+    }
 
 }
 
+
 function l04() {
+    //document.getElementById("numbers1").innerHTML=" "
+
+    for (i = 10; i >=-10; i--){
+        document.getElementById("numbers1").innerHTML += i + " ";
+    }
+    for(i=-20; i<=20; i+=2){
+        document.getElementById("numbers2").innerHTML += i + " ";
+    }
+
+    for(i=29; i>=1; i-=2){
+        document.getElementById("numbers3").innerHTML += i + " ";
+    }
 
 }
 
 function l05() {
+    let min = Number(document.getElementById("min").value)
+    let max = Number(document.getElementById("max").value)
+
+    for (i = min; i<=max; i++){
+        if (i % 3 === 0){
+            document.getElementById("divisibility3").innerHTML += i + " ";
+        }
+
+        if (i % 5 === 0){
+            document.getElementById("divisibility5").innerHTML += i + " ";
+
+        }
+    }
+
 
 }
 
 // copy and paste here the random number generator function 
+
+function getRndInteger(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
 // and use it in the following functions
 
 
 
-function l06() {   
+function l06() {  
+    let zeros = 0;
+    let ones = 0;
+    
+    for (let i = 0; i < 1000; i++) {
+        let digit = getRndInteger(0,1);
+        if (digit === 0){
+            zeros++;
+        }
+        else{
+            ones++;
+        }
+    }
+document.getElementById("zeros").innerHTML = zeros;
+document.getElementById("ones").innerHTML = ones;
+
 
 }
 
